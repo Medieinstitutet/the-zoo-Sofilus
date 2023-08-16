8be3f16c910cdf7f3cb69a3505cf9863f6f5d05c
Sofilus <sofia00olsson@gmail.com>
2023-08-16 13:27:00 +0200

    Created state to check if data allready exists in localstorage

--- src/components/ShowAnimals.tsx
+++ src/components/ShowAnimals.tsx
@@ -1,10 +1,21 @@
 import { useEffect } from "react"
+import { IAnimal } from "../models/IAnimal"
 import { GetAnimals } from "./GetAnimals"
 
 export const ShowAnimals = () => {
 
     useEffect(() => {
-        GetAnimals();
+        // Checkes if data allready existing in localstorage
+        const animalList = localStorage.getItem('AnimalList')
+
+        if(animalList){
+            const parsedAnimalList: IAnimal = JSON.parse(animalList)
+            console.log(parsedAnimalList)
+        }else{
+            console.log('inga sjur')
+           GetAnimals(); 
+        }
+        
     })
 
     return (
