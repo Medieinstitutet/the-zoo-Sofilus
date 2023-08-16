066cec2f55bab30700d192156ce6c3c7eee44e21
Sofilus <sofia00olsson@gmail.com>
2023-08-16 13:08:58 +0200

    Saved my fetched data in localStorage

--- src/components/GetAnimals.ts
+++ src/components/GetAnimals.ts
@@ -4,6 +4,8 @@ import {get} from '../services/AnimalService'
 export const GetAnimals = async (): Promise<IAnimal> => {
     const response = await get<IAnimal>('https://animals.azurewebsites.net/api/animals')
     console.log(response)
-    return response
 
+    localStorage.setItem('AnimalList', JSON.stringify(response))
+
+    return response
 }
