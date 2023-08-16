892523ae8b80cb6c4d5884cebfc62dd8f555d770
Sofilus <sofia00olsson@gmail.com>
2023-08-16 12:38:21 +0200

    Created a component that fetches data from api and shows it in the console

--- src/components/GetAnimals.tsx
+++ src/components/GetAnimals.tsx
@@ -1,9 +1,26 @@
-export const getAnimals = () => {
+import { useEffect, useState } from "react"
+import { IAnimal } from "../models/IAnimal"
+import axios from'axios'
 
-    
+export const GetAnimals = () => {
+
+    useEffect(() => {
+        const getAnimal = async () => {
+            const response = await axios.get<IAnimal>(
+                'https://animals.azurewebsites.net/api/animals'
+            );
+
+           
+            console.log(response.data)
+            console.log('hj')
+
+        } 
+        getAnimal();
+    })
+
     return (
         <>
-
+            <p>hej</p>
         </>
     )
 }
