bff3a43c7d589a06143a054d0653bc82f0d1bb6d
Sofilus <sofia00olsson@gmail.com>
2023-08-17 13:05:42 +0200

    Printing out data about the animals  that will be shown on homepage

--- src/components/ShowAnimals.tsx
+++ src/components/ShowAnimals.tsx
@@ -1,26 +1,37 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { IAnimal } from "../models/IAnimal"
 import { GetAnimals } from "./GetAnimals"
 
 export const ShowAnimals = () => {
 
+    const [animalList, setAnimalList] = useState<IAnimal[]>([]);
+
     useEffect(() => {
         // Checkes if data allready existing in localstorage
-        const animalList = localStorage.getItem('AnimalList')
+       const storedAnimalList = localStorage.getItem('AnimalList')!
 
         if(animalList){
-            const parsedAnimalList: IAnimal = JSON.parse(animalList)
+            const parsedAnimalList: IAnimal[] = JSON.parse(storedAnimalList)
+            setAnimalList(parsedAnimalList)
             console.log(parsedAnimalList)
         }else{
-            console.log('inga sjur')
+            console.log('inga djur')
            GetAnimals(); 
         }
-        
     })
+
+    const animal: JSX.Element[] = animalList.map((animal)=> {return <div> 
+                <img src={animal.imageUrl} />
+                <p>{animal.name}</p>
+                <p>{animal.shortDescription}</p>
+            </div>}) 
 
     return (
         <>
-            <p>hej</p>
+            <div>{animal}</div>
         </>
     )
 }
+
+// Nästa steg är att skapa html för djur korten och sedan måste jag loopa igenom så jag kan skapa flera kort. Frågan är 
+// Om jag ska ja js i min html. Kolla i när man ska printa en lista. Måste kanske mapa en lista eller så kan jag kanske göra en for loop med 15 loopar.
