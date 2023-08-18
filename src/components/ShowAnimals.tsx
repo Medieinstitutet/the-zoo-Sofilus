d37a8f33a595ee9df3231161974b0afd5cd9bf80
Sofilus <sofia00olsson@gmail.com>
2023-08-18 17:32:42 +0200

    when button are clicked the url saves the id of the animal and we move to the component that hadles picked animal.

--- src/components/ShowAnimals.tsx
+++ src/components/ShowAnimals.tsx
@@ -20,14 +20,17 @@ export const ShowAnimals = () => {
         }
     },[])
 
-    const goToAnimal = () => {
-        console.log('test')
+    const goToAnimal = (e: React.MouseEvent) => {
+        let id = (e.currentTarget as HTMLButtonElement).id
+        window.location.href = `http://localhost:5173/${id}`
     }
+
     const animalElements: JSX.Element[] = animalList.map((animal) => (
-        <div id={animal.id.toString()} key={animal.id} onClick={goToAnimal} className="animal-card-container">
+        <div  key={animal.id} className="animal-card-container">
           <img src={animal.imageUrl} className="animal-card-img" alt={animal.name} />
           <p className="animal-card-name">{animal.name}</p>
           <p className="animal-card-description">{animal.shortDescription}</p>
+          <button id={animal.id.toString()} onClick={goToAnimal} >Besök djuret</button>
         </div>
       ));
 
