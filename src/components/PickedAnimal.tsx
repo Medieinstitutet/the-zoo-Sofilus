9b3c9a620768a31e26cc3fa04bcc6de85ca8e2b2
Sofilus <sofia00olsson@gmail.com>
2023-08-19 18:25:57 +0200

    Gets data from localstorage to picked animal component

--- src/components/PickedAnimal.tsx
+++ src/components/PickedAnimal.tsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+import { IAnimal } from "../models/IAnimal";
+import { GetAnimals } from "./GetAnimals";
 
-export const PickedAnimal = () => {
+export const PrintPickedAnimal = () => {
+    const [animalList, setAnimalList] = useState<IAnimal[]>([]);
+
+    useEffect(() => {
+        // Checkes if data allready existing in localstorage
+       const storedAnimalList = localStorage.getItem('AnimalList')!
+
+        if(storedAnimalList){
+            const parsedAnimalList: IAnimal[] = JSON.parse(storedAnimalList)
+            setAnimalList(parsedAnimalList)
+            console.log(parsedAnimalList)
+        }else{
+            console.log('inga djur')
+           GetAnimals(); 
+        }
+    },[])
+
+
     return (
         <>test
         </>
