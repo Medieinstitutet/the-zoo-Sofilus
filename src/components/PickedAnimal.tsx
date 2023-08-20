d1a5919e990f9ae468e997a73e3d02d1a79967be
Sofilus <sofia00olsson@gmail.com>
2023-08-20 09:24:14 +0200

    Added html for picked aninmal

--- src/components/PickedAnimal.tsx
+++ src/components/PickedAnimal.tsx
@@ -20,18 +20,32 @@ export const PrintPickedAnimal = () => {
         }
     },[])
 
-
     const { id } = useParams<{ id: string }>();
 
     const foundAnimal = animalList.find((animal) => animal.id.toString() === id);
     console.log(foundAnimal)
-        
-        
-    
 
+    let htmlElements: JSX.Element | null = null;
+    if (foundAnimal) {
+      htmlElements = (
+        <div key={foundAnimal.id}>
+            <h1>{foundAnimal.name}</h1>
+            <img src={foundAnimal.imageUrl} className="animal-card-img" alt={foundAnimal.name}/>
+            <p>Födelseår: {foundAnimal.yearOfBirth}</p>
+            <p className="animal-card-description">{foundAnimal.longDescription}</p>
+            <p>Matades senast: {foundAnimal.lastFed}</p>
+            <button>Mata {foundAnimal.name}</button>
+        </div>
+      );
+    } else {
+      htmlElements = <p>Djuret kunde inte hittas</p>;
+    }
+
+    // jag ska bara skriva ut rätt html
 
     return (
-        <>test
+        <>
+           <div>{htmlElements}</div>
         </>
     
     )
