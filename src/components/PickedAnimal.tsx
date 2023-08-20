0f01b4162db6c2b4993b57635fa2f6ef1d79c6c2
Sofilus <sofia00olsson@gmail.com>
2023-08-20 09:40:55 +0200

    Replaced code that gets aniamllist from localstorage with a functioncall

--- src/components/PickedAnimal.tsx
+++ src/components/PickedAnimal.tsx
@@ -1,24 +1,8 @@
-import { useState, useEffect } from "react";
-import { IAnimal } from "../models/IAnimal";
-import { GetAnimals } from "./GetAnimals";
 import { useParams } from "react-router-dom";
+import { GetAnimalsFromLocalstorage } from "./GetAnimalFromLocalstorage";
 
 export const PrintPickedAnimal = () => {
-    const [animalList, setAnimalList] = useState<IAnimal[]>([]);
-
-    useEffect(() => {
-        // Checkes if data allready existing in localstorage
-       const storedAnimalList = localStorage.getItem('AnimalList')!
-
-        if(storedAnimalList){
-            const parsedAnimalList: IAnimal[] = JSON.parse(storedAnimalList)
-            setAnimalList(parsedAnimalList)
-            console.log(parsedAnimalList)
-        }else{
-            console.log('inga djur')
-           GetAnimals(); 
-        }
-    },[])
+   const animalList = GetAnimalsFromLocalstorage();
 
     const { id } = useParams<{ id: string }>();
 
