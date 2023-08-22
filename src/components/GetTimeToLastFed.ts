25b589f279704d4f6f1ac7cf10678d8f03534490
Sofilus <sofia00olsson@gmail.com>
2023-08-22 11:28:17 +0200

    Rewrote code for get time

--- src/components/GetTimeToLastFed.ts
+++ src/components/GetTimeToLastFed.ts
@@ -1,9 +1,5 @@
 export const GetCurrentTime = () => {
 
-    const today = new Date();
-    const hour = today.getHours();
-    const minutes = today.getMinutes();
-    const seconds = today.getSeconds();
-    const currentTime = `${hour}:${minutes}:${seconds}`;
-    return currentTime
+    const today = new Date();;
+    return today
 }
