d0c00c9fb4d6c4dcc08b2f78049051a5ad08ace6
Sofilus <sofia00olsson@gmail.com>
2023-08-22 11:28:58 +0200

    rewriote the getanimalfrom localstorage

--- src/services/GetAnimalFromLocalstorage.ts
+++ src/services/GetAnimalFromLocalstorage.ts
@@ -6,16 +6,19 @@ import { GetAnimals } from "../components/GetAnimals";
 export const GetAnimalsFromLocalstorage = async () => {
 
     let animalList: IAnimal[] =[]
-
-    await GetAnimals();
+    
+    
     const storedAnimalList = localStorage.getItem('AnimalList')!
 
         if(storedAnimalList){
+            
             animalList = JSON.parse(storedAnimalList)
             
         }else{
             console.log('inga djur')
-        GetAnimals(); 
+            await GetAnimals(); 
+            const storedAnimalList = localStorage.getItem('AnimalList')!
+            animalList = JSON.parse(storedAnimalList)
         }
 
 return animalList
