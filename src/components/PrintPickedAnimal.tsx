8c4d5809a94236fc69f4c906522de14cc8b9261f
Sofilus <sofia00olsson@gmail.com>
2023-08-22 14:05:12 +0200

    Moved html for picked animal to an own component

--- src/components/PrintPickedAnimal.tsx
+++ src/components/PrintPickedAnimal.tsx
@@ -2,6 +2,7 @@ import { GetPickedAnimal } from "./GetPickedAnimal";
 import { useState, useEffect } from "react";
 import { IAnimal } from "../models/IAnimal";
 import { GetAnimalsFromLocalstorage } from "../services/GetAnimalFromLocalstorageService";
+import { HtmlPickedAnimal } from "./HtmlPickedAnimal";
 
 export const PrintPickedAnimal = () => {
   
@@ -72,24 +73,13 @@ export const PrintPickedAnimal = () => {
     localStorage.setItem('LastFedTime', currentTime.getTime().toString());
   }
   }
-
-  let htmlElements: JSX.Element | null = null;
-  if (foundAnimal) {
-    htmlElements = (
-      <div key={foundAnimal.id} className="picked-animal-container">
-          <h1 className="picked-animal-name">{foundAnimal.name}</h1>
-          <img src={foundAnimal.imageUrl} alt={foundAnimal.name} className="picked-animal-img"/>
-          <p className="picked-animal-year-of-birth">Födelseår: {foundAnimal.yearOfBirth}</p>
-          <p className="picked-animal-description">{foundAnimal.longDescription}</p>
-          <p className="picked-animal-last-fed"> Matades senast: {newFeedTime.toString() }</p>
-          <button disabled={isFed} className="picked-animal-feed-btn" onClick={handleFeedAnimal} id={foundAnimal.id.toString()}>Mata {foundAnimal.name}</button>
-      </div>
-    );
-  } else {
-    htmlElements = <p>Djuret kunde inte hittas</p>;
-  }
   
   return (
-          <div className="picked-animal-page">{htmlElements}</div>
+    <>
+      <HtmlPickedAnimal 
+      newFeedTime={newFeedTime}
+        isFed={isFed}
+        handleFeedAnimal={handleFeedAnimal}></HtmlPickedAnimal>
+    </>      
   )
 }
