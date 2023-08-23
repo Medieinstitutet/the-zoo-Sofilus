5adcf78d42d90de921c4e6ac0dcb706335ba63c1
Sofilus <sofia00olsson@gmail.com>
2023-08-23 16:11:40 +0200

    Made animal card change color when its hungry and change back when its feed

--- src/components/PrintAnimals.tsx
+++ src/components/PrintAnimals.tsx
@@ -5,16 +5,27 @@ import { IAnimal } from "../models/IAnimal";
 export const PrintAnimals = () => {
 
     const [animalList, setAnimalList] = useState<IAnimal[]>([]);
-  
+    const timerThreeHours = 10 * 1000;
+
     const fetchedData = async () =>{
         const animalListLocal = await GetAnimalsFromLocalstorage() 
         if(animalListLocal){
             setAnimalList(animalListLocal)
         }  
     }
-    
+
     useEffect(() => {
         fetchedData();
+
+        animalList.map((animal) => {
+            if (((new Date()).getTime() - (new Date(animal.lastFed)).getTime()) < timerThreeHours) {
+               animal.isFed = false;
+            } else {
+                animal.isFed = true;
+            }
+        
+            return animal;
+          });
         
     },[]) 
 
@@ -23,8 +34,10 @@ export const PrintAnimals = () => {
         window.location.href = `http://localhost:5173/${id}`
     }
 
+  
+
     const animalElements: JSX.Element[] = animalList.map((animal) => (
-        <div  key={animal.id} className={`animal-card-container ${animal.isFed ? '' : 'hungry'}`}>
+        <div  key={animal.id} className={`animal-card-container ${((new Date()).getTime() - (new Date(animal.lastFed)).getTime()) < timerThreeHours  ? '' : 'hungry'}`}>
             <img src={animal.imageUrl} className="animal-card-img" alt={animal.name} />
             <p className="animal-card-name">{animal.name}</p>
             <p className="animal-card-description">{animal.shortDescription}</p>
